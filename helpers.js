95fea5b5addbec5dd32e887d1f53795e4a4f1aef
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-12-20 14:38:15 -0500

    Aberdeen Mall 2.0 1.0  /helpers.js

--- helpers.js
+++ helpers.js
@@ -75,6 +75,7 @@ function init(e){
     $('#option_selector').change(function(){
         window.location = $(this).val();
     });
+    
     $('.link_selector').change(function(){
         window.location = $(this).val();
     });
@@ -259,7 +260,7 @@ function floorList() {
     floor_1.id = "first-floor";
     floor_1.title = "Level One";
     floor_1.map =  getPNGMapURL().split("?")[0];
-    floor_1.z_index = 1;
+    floor_1.z_index = null;
     floor_1.show = true;
     floor_list.push(floor_1);
     return floor_list;
