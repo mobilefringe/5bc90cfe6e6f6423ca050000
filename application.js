57de1963de6beef455a4f4916571b44436a897af
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-04-02 12:16:35 -0400

    Aberdeen Mall 2.0 1.3  /application.js

--- application.js
+++ application.js
@@ -152,9 +152,9 @@ function renderFeatureItems(){
     $.each(items, function(i, val){
         if (val.name === "") {
             if(val.url === ""){
-                $('#feature_' + i).html('<img src="'+ val.image_url+'" class="hoverer" alt="Feature Item">')
+                $('#feature_' + i).html('<img src="'+ val.image_url+'" class="hoverer" alt="Feature Item ' +val.id+ '">')
             } else {
-                $('#feature_' + i).html('<a href="'+ val.url +'"><img src="'+ val.image_url+'" class="hoverer" alt="Feature Item"></a>')
+                $('#feature_' + i).html('<a href="'+ val.url +'"><img src="'+ val.image_url+'" class="hoverer" alt="Feature Item ' +val.id+ '"></a>')
             }
         } else {
             $('#feature_' + i).html('<a href="'+ val.url +'"><img src="'+ val.image_url+'" class="hoverer" alt="' +val.name+ '"><h5 class="center_h">'+ val.name +'</h5></a>')
